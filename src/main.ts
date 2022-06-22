6f25a2d9d7d25857db2713dea3408859297461df
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-06-22 16:48:55 -0500

    chore(Root): Add linter

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
-import './styles.css'
-import viteLogo from '/vite.svg'
 import { setupCounter } from './counter'
+
+import './styles.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
+    <h1>Design Comparison</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
