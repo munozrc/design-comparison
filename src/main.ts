24e45fd58f46c3770781f1ae1effd91eaddded47
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-06-23 21:03:09 -0500

    fix(Root): Fix iframe scaling

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import './styles.css'
 
-const container: HTMLDivElement | null = document.querySelector('#container')
+const container = document.querySelector('#container') as HTMLDivElement
 
 let width: number
 let offsetLeft: number
 let isDown: boolean = false
 
-container?.addEventListener('mousedown', (e) => {
+container.addEventListener('mousedown', (e) => {
   width = container.offsetWidth
   offsetLeft = container.offsetLeft
   isDown = true
@@ -24,6 +24,23 @@ document.addEventListener('mousemove', (e) => {
 function updateDragger (e: MouseEvent) {
   if (isDown && e.pageX >= offsetLeft && e.pageX <= (offsetLeft + width)) {
     const value = e.pageX - offsetLeft - 3
-    container?.style.setProperty('--percent', value + 'px')
+    container.style.setProperty('--percent', value + 'px')
   }
 }
+
+function init () {
+  const design = document.querySelector('#design') as HTMLImageElement
+  const solution = document.querySelector('#solution') as HTMLIFrameElement
+
+  const { width, height, naturalWidth, naturalHeight } = design
+  const scaleX = width / naturalWidth
+  const scaleY = height / naturalHeight
+
+  solution.style.transform = `scale(${scaleX}, ${scaleY})`
+  solution.style.width = naturalWidth + 'px'
+  solution.style.height = naturalHeight + 'px'
+
+  console.log({ naturalWidth, naturalHeight })
+}
+
+init()
