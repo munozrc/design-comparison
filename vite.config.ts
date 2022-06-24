ab4ad6f7abf8a6c641a5b3ea36fc2a25b854d223
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-06-23 22:14:12 -0500

    feat(Root): Add config for deploy

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import path from 'node:path'
+import { defineConfig } from 'vite'
+
+const isGitHubPages = true
+const folderName = path.basename(process.cwd()) + '/'
+const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development'
+const base = mode === 'production' && isGitHubPages ? '/' + folderName : '/'
+
+export default defineConfig({
+  root: 'src',
+  base,
+  mode,
+  publicDir: '../public',
+  build: {
+    outDir: '../dist',
+    assetsDir: './'
+  }
+})
