da71cb57c90961e167467f44c5c508b5b6be4d11
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-06-29 17:13:15 -0500

    fix(Root): send data file reader

--- src/lib/FieldDropImage.svelte
+++ src/lib/FieldDropImage.svelte
@@ -1,13 +1,11 @@
 <script>
+  import { createEventDispatcher } from 'svelte'
   import UploadIcon from '../icons/Upload.svelte'
 
-  function handleDrop (event) {
-    event.stopPropagation()
-    event.preventDefault()
+  let file
 
-    const fileList = event.dataTransfer.files
-    console.log({ fileList })
-  }
+  const dispatch = createEventDispatcher()
+  const reader = new FileReader()
 
   function handleDragOver (event) {
     event.stopPropagation()
@@ -15,20 +13,44 @@
     event.dataTransfer.dropEffect = 'copy'
   }
 
+  function handleDrop (event) {
+    event.stopPropagation()
+    event.preventDefault()
+
+    const fileList = event.dataTransfer.files
+    handleGetFile(fileList[0])
+  }
+
   function handleChange (event) {
     const fileList = event.target.files
-    console.log(fileList)
+    handleGetFile(fileList[0])
+  }
+
+  function handleGetFile (data) {
+    if (!data.type.match('image.*')) {
+      file = undefined
+      return
+    }
+
+    file = data.name
+  
+    reader.readAsDataURL(data)
+    reader.onload = e => dispatch('file', { data: e.target.result })
   }
 </script>
 
 <div class="wrapper" on:drop={handleDrop} on:dragover={handleDragOver}>
   <div class="container">
     <UploadIcon size="62px" color="#92b0e7"/>
-    <input type="file" id="file" on:change={handleChange} accept="image/*"/>
-    <label for="file">
-      <strong>Choose a file</strong>
-      <span> or drag it here</span>
-    </label>
+    {#if (typeof file === 'undefined')}
+      <input type="file" id="file" on:change={handleChange} accept="image/*"/>
+      <label for="file">
+        <strong>Choose a file</strong>
+        <span> or drag it here</span>
+      </label>
+    {:else}
+      <strong>{file}</strong>
+    {/if}
   </div>
 </div>
 
@@ -40,7 +62,7 @@
     place-items: center;
     outline: 2px dashed #6997eb;
     outline-offset: -10px;
-    padding: 100px 20px;
+    padding: 100px 0px;
     border-radius: 4px;
     background-color: #eee;
   }
