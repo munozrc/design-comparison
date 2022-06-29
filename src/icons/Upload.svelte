946aa4effffef405242032bc6f76994aa19b81f9
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-06-29 15:16:07 -0500

    feat(Root): Create new icon

---
+++ src/icons/Upload.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let size = '1em'
+  export let color = 'currentColor'
+</script>
+
+<svg 
+  stroke={color}
+  fill="none" 
+  stroke-width="2" 
+  viewBox="0 0 24 24" 
+  stroke-linecap="round" 
+  stroke-linejoin="round" 
+  height={size} 
+  width={size}
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+  <polyline points="17 8 12 3 7 8"></polyline>
+  <line x1="12" y1="3" x2="12" y2="15"></line>
+</svg>
