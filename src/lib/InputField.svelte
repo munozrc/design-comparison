65a0f8794065e1eff67f9e81777a861ea63b74e2
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-06-29 19:36:41 -0500

    feat(Root): Create new InputField

---
+++ src/lib/InputField.svelte
@@ -0,0 +1,33 @@
+<script>
+  export let placeholder
+  export let value
+  export let type
+  export let label
+</script>
+
+<label>
+  {label}
+  <input {type} {value} {placeholder} on:keyup />
+</label>
+
+<style>
+  label {
+    display: flex;
+    flex-direction: column;
+  }
+
+  input {
+    appearance: none;
+    background-color: #fff;
+    border: 1px solid #eee;
+    border-radius: 6px;
+    box-shadow: none;
+    caret-color: #5a5e9a;
+    color: #23263b;
+    flex: 1;
+    font: inherit;
+    max-width: 100%;
+    padding-left: 2.5rem;
+    margin-right: 1rem;
+  }
+</style>
