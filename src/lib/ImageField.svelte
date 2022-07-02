ba1c6d9e41a47913104c5fc30f31cf6a6078ad20
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-07-02 11:45:14 -0500

    feat(Root): Add drag and input file support

--- src/lib/ImageField.svelte
+++ src/lib/ImageField.svelte
@@ -1,23 +1,58 @@
 <script>
   import UploadIcon from '../icons/Upload.svelte'
+  import CloseIcon from '../icons/Close.svelte'
 
   export let label = 'Your desktop design'
+
+  let files
+  let inputRef
+
+  const types = ['image/png', 'image/jpeg']
+
+  function handleDragOver (event) {
+    event.stopPropagation()
+    event.preventDefault()
+    event.dataTransfer.dropEffect = 'copy'
+  }
+
+  function handleDrop (event) {
+    event.stopPropagation()
+    event.preventDefault()
+    const file = event.dataTransfer.files[0]
+    const isTypeValid = types.includes(file.type)
+
+    if (isTypeValid) files = event.dataTransfer.files
+  }
+
+  function resetState () {
+    inputRef.value = ''
+    files = undefined
+  }
 </script>
 
-<label for="file">
+<section>
   {label}
-  <div>
-    <button>
+  <div on:drop={handleDrop} on:dragover={handleDragOver}>
+    <button class="btn-upload" on:click={() => { inputRef.click() }}>
       <UploadIcon />
     </button>
-    <input type="file" id="file" accept="image/*"/>
-    <span>or drag it here</span>
+    <input type="file" accept={types.join(', ')} bind:files bind:this={inputRef} />
+    <span>
+      {#if files && files[0]}
+      {files[0].name}
+      <button class="btn-clear" on:click={resetState}>
+        <CloseIcon />
+      </button>
+      {:else}
+      or drag it here
+      {/if}
+    </span>
   </div>
-</label>
+</section>
 
 <style>
 
-  label {
+  section {
     width: 100%;
     display: flex;
     flex-direction: column;
@@ -36,7 +71,7 @@
     border-radius: 6px;
   }
 
-  button {
+  .btn-upload {
     height: 100%;
     aspect-ratio: 1;
     border: none;
@@ -47,14 +82,39 @@
     align-items: center;
     font-size: 1.2em;
     border-radius: 6px;
+    transition: background-color 0.15s ease-in-out;
+  }
+
+  .btn-upload:hover {
+    background-color: #383838;
   }
 
   span {
-    display: inline-block;
-    padding: 10px 24px;
+    width: 100%;
+    position: relative;
+    padding: 10px 4px 10px 18px;
     font-size: 1.1em;
-    line-height: normal;
-    text-align: start;
     color: #757575;
+  }
+
+  .btn-clear {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 50%;
+    right: -6px;
+    background-color: transparent;
+    border: none;
+    font-size: 1.05em;
+    color: #101010;
+    padding: 5px;
+    border-radius: 50%;
+    transform: translate(-50%, -50%);
+    transition: background-color 0.15s ease-in-out;
+  }
+
+  .btn-clear:hover {
+    background-color: #bdbdbd;
   }
 </style>
