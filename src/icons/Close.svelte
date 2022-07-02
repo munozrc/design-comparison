309c1f93dded38c5f910fd4d02ef2ab648947242
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-07-02 10:43:06 -0500

    feat(Root): Add new icon

---
+++ src/icons/Close.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let size = '1em'
+  export let color = 'currentColor'
+</script>
+
+<svg 
+  stroke={color}
+  fill="none" 
+  stroke-width="2" 
+  viewBox="0 0 24 24" 
+  stroke-linecap="round" 
+  stroke-linejoin="round" 
+  height={size} 
+  width={size}
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <circle cx="12" cy="12" r="10"></circle>
+  <line x1="15" y1="9" x2="9" y2="15"></line>
+  <line x1="9" y1="9" x2="15" y2="15"></line>
+</svg>
